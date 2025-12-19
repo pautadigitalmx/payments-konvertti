2cbaa583fc9064f96cc96a44c99b05d87a3092d1
pautadigitalmx <49472137+pautadigitalmx@users.noreply.github.com>
2025-12-19 04:01:10 -0600

    Update fallback Postgres connection string

--- app/db.server.js
+++ app/db.server.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const databaseUrl =
   process.env.DATABASE_URL ||
-  "postgres://069ed3512f523cfbaa74d69d0709ab6fe158a25d622e821310d12347a731e10b:sk_SSBDWSjidHUu8G2ffIN6K@db.prisma.io:5432/postgres?sslmode=require";
+  "postgres://069ed3512f523cfbaa74d69d0709ab6fe158a25d622e821310d12347a731e10b:sk_ka5js4A-9qV1FTRK6sTq_@db.prisma.io:5432/postgres?sslmode=require";
 
 if (!process.env.DATABASE_URL) {
   process.env.DATABASE_URL = databaseUrl;
